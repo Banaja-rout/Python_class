ec80b3c9bfe4e3e0c84656bf69fedae5289fcbe7
Banaja-rout <146611803+Banaja-rout@users.noreply.github.com>
2023-12-23 14:10:45 +0530

    Update Banaja.py

--- Day3/Banaja.py
+++ Day3/Banaja.py
@@ -1,1 +1,9 @@
-
+list=[]
+n=int(input("Enter Your limit: "))
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+             x=input()
+             list.append(x)
+              
+print(list)
